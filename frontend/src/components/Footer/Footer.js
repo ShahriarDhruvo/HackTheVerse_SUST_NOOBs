c9f68e5ab1e788429cbf1956934d178228335a8b
sania51 <proshirahman@gmail.com>
2020-11-14 21:47:59 +0600

    footer fix

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -6,71 +6,67 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="main-footer">
-      <div className="container">
+      <div className="container inner">
         <div className="row  justify-content-center">
-          <div className="col-sm-6">
-            <div className="row">
-              <div className="col">
-                <Link to={`/`}>
-                  <img
-                    src={"/static/img/github.png"}
-                    width={"40"}
-                    height={"40"}
-                    className={"d-inline-block align-top"}
-                    className="github"
-                    alt={"logo"}
-                  />
-                </Link>
-              </div>
-              <div className="col">
-                <Link to={`/`}>
-                  <img
-                    src={"/static/img/linkedin.png"}
-                    width={"40"}
-                    height={"40"}
-                    className={"d-inline-block align-top"}
-                    className="github"
-                    alt={"logo"}
-                  />
-                </Link>
-              </div>
-              <div className="col">
-                <Link to={`/`}>
-                  <img
-                    src={"/static/img/stackoverflow.png"}
-                    width={"40"}
-                    height={"40"}
-                    className={"d-inline-block align-top"}
-                    className="github"
-                    alt={"logo"}
-                  />
-                </Link>
-              </div>
-              <div className="col">
-                <Link to={`/`}>
-                  <img
-                    src={"/static/img/facebook.png"}
-                    width={"40"}
-                    height={"40"}
-                    className={"d-inline-block align-top"}
-                    className="github"
-                    alt={"logo"}
-                  />
-                </Link>
-              </div>
-              <div className="col">
-                <Link to={`/`}>
-                  <img
-                    src={"/static/img/twitter.png"}
-                    width={"40"}
-                    height={"40"}
-                    className={"d-inline-block align-top"}
-                    className="github"
-                    alt={"logo"}
-                  />
-                </Link>
-              </div>
-            </div>
+          <div className="col-auto">
+            <Link to={`/`}>
+              <img
+                src={"/static/img/github.png"}
+                width={"40"}
+                height={"40"}
+                className={"d-inline-block align-top"}
+                className="github"
+                alt={"logo"}
+              />
+            </Link>
+          </div>
+          <div className="col-auto">
+            <Link to={`/`}>
+              <img
+                src={"/static/img/linkedin.png"}
+                width={"40"}
+                height={"40"}
+                className={"d-inline-block align-top"}
+                className="github"
+                alt={"logo"}
+              />
+            </Link>
+          </div>
+          <div className="col-auto">
+            <Link to={`/`}>
+              <img
+                src={"/static/img/stackoverflow.png"}
+                width={"40"}
+                height={"40"}
+                className={"d-inline-block align-top"}
+                className="github"
+                alt={"logo"}
+              />
+            </Link>
+          </div>
+          <div className="col-auto">
+            <Link to={`/`}>
+              <img
+                src={"/static/img/facebook.png"}
+                width={"40"}
+                height={"40"}
+                className={"d-inline-block align-top"}
+                className="github"
+                alt={"logo"}
+              />
+            </Link>
+          </div>
+          <div className="col-auto">
+            <Link to={`/`}>
+              <img
+                src={"/static/img/twitter.png"}
+                width={"40"}
+                height={"40"}
+                className={"d-inline-block align-top"}
+                className="github"
+                alt={"logo"}
+              />
+            </Link>
           </div>
         </div>
         <hr />
