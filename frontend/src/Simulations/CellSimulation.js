723827978782953376c70e1314b5107bdaa0656d
Shahriar_Dhruvo <42747800+ShahriarDhruvo@users.noreply.github.com>
2020-11-15 00:05:48 +0600

    Simulators introduced

---
+++ frontend/src/Simulations/CellSimulation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Unity, { UnityContent } from "react-unity-webgl";
+
+const CellSimulation = () => {
+    const unityContent = new UnityContent(
+        "/static/Simulators/CellSimulation/Build/CellSimulation.json",
+        "/static/Simulators/CellSimulation/Build/UnityLoader.js"
+    );
+
+    return (
+        <div>
+            <Unity unityContent={unityContent} />
+        </div>
+    );
+};
+
+export default CellSimulation;
